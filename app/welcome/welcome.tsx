2653fa42d3498da9961cd55a4e2523d30baa570a
Thiago Assunção <161776524+ittoogami@users.noreply.github.com>
2026-01-28 00:41:13 +0000

    Criação inicial

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,12 @@
+export function Welcome() {
+  const titulo = "Hello World";
+  const resultado = 10 + 5;
+  return (
+    <div className="header">
+      <h1>
+        {titulo}
+      </h1>
+    </div>
+    
+  );
+}
